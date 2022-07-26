74d2a8258ebac216b9bb679f9b917a02a94b4697
christopher chapa <chapachristopher16@gmail.com>
2022-07-25 20:10:19 -0500

    models uploaded to database

--- champion_or_chump/api/admin.py
+++ champion_or_chump/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post, Comments
+
+# Register your models here
+admin.site.register(Post)
+admin.site.register(Comments)
--- champion_or_chump/api/models.py
+++ champion_or_chump/api/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+from datetime import date, datetime
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100)
+    content = models.TextField()
+    likes = models.IntegerField(0, default=0)
+    posted = models.DateField(default=datetime.now)
+
+    class Meta:
+        ordering = ('-posted',)
+
+        def __str__(self):
+            return self.subject
+
+class Comments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE )
+    comment = models.TextField()
+    likes = models.IntegerField(0, default=0)
+    posted = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        ordering = ('-posted',)
+
+        def __str__(self):
+            return self.comment
+
